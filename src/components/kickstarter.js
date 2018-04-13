9eaa56024cd9a4b2d8fdc3f70d6ad6d4a89f257e
jcstorms1 <jcstorms25@gmail.com>
2018-04-13 15:37:40 -0400

    created countdown

--- src/components/kickstarter.js
+++ src/components/kickstarter.js
@@ -1,14 +1,61 @@
-import React from "react";
-import Countdown from "react-countdown-now";
+import React, { Component } from "react";
+import Moment from "moment";
 import "../stylesheets/kickstarter.css";
 
-const KickStarter = () => {
-  return (
-    <section className="kickstarter">
-      <h1>Kickstarter Coming Soon!</h1>
-      <Countdown date={Date.now() + 1000000} daysInHours={true} />
-    </section>
-  );
-};
+class KickStarter extends Component {
+  state = {
+    days: 0,
+    hours: 0,
+    mins: 0,
+    seconds: 0,
+    intervalId: null,
+    started: false,
+    deadline: "2018-05-31 23:59:59",
+    launched: false
+  };
+
+  timeLeft = () => {
+    const end = Moment(this.state.deadline);
+    const now = Moment(new Date());
+    if (end.diff(now) < 0) {
+      this.setState({ launched: true });
+    } else {
+      const seconds = end.diff(now, "seconds") % 60;
+      const mins = end.diff(now, "minutes") % 60;
+      const hours = end.diff(now, "hours") % 24;
+      const days = end.diff(now, "days");
+      this.setState({ days, hours, mins, seconds });
+    }
+  };
+  componentDidMount() {
+    const intervalId = setInterval(() => this.timeLeft(), 1000);
+    this.setState({ started: true });
+    this.setState({ intervalId });
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.state.intervalId);
+  }
+
+  render() {
+    console.log(this.state.intervalId);
+    return (
+      <section className="kickstarter">
+        <h1>Kickstarter Coming June 1 st 2018!</h1>
+
+        {this.state.started && !this.state.launched ? (
+          "Calculating..."
+        ) : (
+          <div className="clock">
+            <div className="days">{this.state.days} Days</div>
+            <div>{this.state.hours} Hours</div>
+            <div>{this.state.mins} Min(s)</div>
+            <div>{this.state.seconds} Sec(s)</div>
+          </div>
+        )}
+      </section>
+    );
+  }
+}
 
 export default KickStarter;
