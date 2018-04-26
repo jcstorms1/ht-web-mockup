3ae1ffdc9d2bb4a4351fbe17ae1596622f3bcdcd
jcstorms1 <jcstorms25@gmail.com>
2018-04-26 11:21:48 -0400

    further clean up and fixing some html layout

--- src/components/team.js
+++ src/components/team.js
@@ -6,7 +6,7 @@ const Team = () => {
   return (
     <div className="team-wrapper">
       <div className="cover">
-        <div className="team">
+        <div className="our-team">
           <h1>Our Team</h1>
           <p>
             The founders of Hatchtrack, Michael and Meghan, live in Yacolt WA
@@ -15,28 +15,28 @@ const Team = () => {
             believe in the value of animal life the same as we believe in the
             value of human life!
           </p>
-          <br />
-          <section className="ceo">
-            <div className="ceo-image">
-              <img src={CEO} alt="CEO" />
-            </div>
-            <div className="ceo-bio">
-              <p>
-                Michael is CEO of a small engineering contracting company called
-                Widgt, specializing in high quality electronic development
-                across a wide variety of industries. He’s worked in technology
-                in a diverse range of industries, including process equipment,
-                defense, IT, automotive, radio, and consumer electronics. He
-                loves escaping technology as well, and has backpacked many parts
-                of the Appalachian Trail, Glacier National Park, and the Sacred
-                Valley in Peru. Beyond egg incubation, he also wants to found a
-                combination tech incubator and monastery, which he
-                affectionately refers to as a space cult - will he be the first
-                to keep chickens on Mars?
-              </p>
-            </div>
-          </section>
-          <br />
+        </div>
+        <div className="ceo">
+          <div className="ceo-image">
+            <img src={CEO} alt="CEO" />
+          </div>
+          <div className="ceo-bio">
+            <h1>Michael Anfang</h1>
+            <p>
+              Michael is CEO of a small engineering contracting company called
+              Widgt, specializing in high quality electronic development across
+              a wide variety of industries. He’s worked in technology in a
+              diverse range of industries, including process equipment, defense,
+              IT, automotive, radio, and consumer electronics. He loves escaping
+              technology as well, and has backpacked many parts of the
+              Appalachian Trail, Glacier National Park, and the Sacred Valley in
+              Peru. Beyond egg incubation, he also wants to found a combination
+              tech incubator and monastery, which he affectionately refers to as
+              a space cult - will he be the first to keep chickens on Mars?
+            </p>
+          </div>
+        </div>
+        <div className="ceo-2">
           <p>
             Meghan is currently a stay at home mom for her two daughters and a
             natural birth advocate. She has been involved in the wedding
