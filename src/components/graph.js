b9862949e6c7488dcfd6bf7464f802c914fbedab
jcstorms1 <jcstorms25@gmail.com>
2018-05-01 21:41:56 -0400

    added initial styling to charts

--- src/components/graph.js
+++ src/components/graph.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
+import moment from "moment";
 
 const Graph = props => {
   let labels = [];
@@ -8,7 +9,7 @@ const Graph = props => {
   let pressure = [];
 
   for (let obj of props.data.data) {
-    labels.push(obj["timestamp"]);
+    labels.push(moment(obj["timestamp"]).format("LT"));
     temperature.push(obj["temperature"]);
     humidity.push(obj["humidity"]);
     pressure.push(obj["pressure"]);
@@ -20,23 +21,52 @@ const Graph = props => {
       {
         label: "Temperature",
         data: temperature,
-        fill: false
+        fill: false,
+        backgroundColor: "rgba(63, 127, 191, 0.4)",
+        borderColor: "rgba(63, 127, 191, 1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 4,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(63, 127, 191, 1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10
       },
       {
         label: "Humidity",
         data: humidity,
-        fill: false
+        fill: false,
+        backgroundColor: "rgba(241, 113, 8, 0.4)",
+        borderColor: "rgba(241, 113, 8, 1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 4,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(241, 113, 8, 1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10
       }
     ]
   };
-
   const pressureData = {
     labels: labels,
     datasets: [
       {
         label: "Pressure",
         data: pressure,
-        fill: false
+        fill: false,
+        backgroundColor: "rgba(12, 170, 83, 0.4)",
+        borderColor: "rgba(12, 170, 83, 1)",
+        pointBackgroundColor: "#fff",
+        pointBorderWidth: 4,
+        pointHoverRadius: 5,
+        pointHoverBackgroundColor: "rgba(12, 170, 83, 1)",
+        pointHoverBorderColor: "rgba(220,220,220,1)",
+        pointHoverBorderWidth: 2,
+        pointRadius: 1,
+        pointHitRadius: 10
       }
     ]
   };
