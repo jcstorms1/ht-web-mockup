f930378ac4c24c4d9c9b1468670de4149303238f
jcstorms1 <jcstorms25@gmail.com>
2018-05-03 15:54:47 -0400

    add subscription form

---
+++ src/components/footer/subscribe.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Subscribe = () => {
+  return (
+    <section>
+      <form>
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Last Name" />
+        <input type="email" placeholder="Email" />
+        <button>Send</button>
+      </form>
+    </section>
+  );
+};
+
+export default Subscribe;
--- src/containers/footer.js
+++ src/containers/footer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import ContactUs from "../components/footer/contactus";
 import SocialMedia from "../components/footer/socialmedia";
+import Subscribe from "../components/footer/subscribe";
 import "../stylesheets/footer.css";
 import logo from "../images/logo-typeonly.png";
 
@@ -11,14 +12,16 @@ class Footer extends Component {
         <div className="inner-bottom">
           {/* <img src={logo} alt="logo" /> */}
           <div className="social-container">
-            <h1>Follow Us...</h1>
-            <SocialMedia />
+            <h1>Subscribe To Our Mailing List</h1>
+            <Subscribe />
           </div>
           <div className="form-container">
             <h1>Contact Us</h1>
             <ContactUs />
           </div>
         </div>
+        <h1>Follow Us...</h1>
+        <SocialMedia />
       </div>
     );
   }
