e3bc32f2c5dafd521694ea1b7ce2defaf6b51d31
jcstorms1 <jcstorms25@gmail.com>
2018-05-03 20:12:13 -0400

    added sub footer for privacy links

---
+++ src/components/footer/subFooter.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SubFooter = () => {
+  return (
+    <div>
+      <p>Copyright © Hatchtrack 2018</p>
+    </div>
+  );
+};
+
+export default SubFooter;
--- src/containers/footer.js
+++ src/containers/footer.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import ContactUs from "../components/footer/contactus";
 import SocialMedia from "../components/footer/socialmedia";
 import Subscribe from "../components/footer/subscribe";
+import SubFooter from "../components/footer/subFooter";
 import "../stylesheets/footer.css";
 
 class Footer extends Component {
@@ -9,9 +10,6 @@ class Footer extends Component {
     return (
       <div className="wrapper-bottom">
         <div className="inner-bottom">
-          <div className="social-container">
-            <SocialMedia />
-          </div>
           <div className="subscribe-container">
             <Subscribe />
           </div>
@@ -19,7 +17,11 @@ class Footer extends Component {
             <h1>Contact Us</h1>
             <ContactUs />
           </div>
+          <div className="social-container">
+            <SocialMedia />
+          </div>
         </div>
+        <SubFooter />
       </div>
     );
   }
